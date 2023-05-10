0b916b896f70d287358d4bf3a2727bb8cbd79749
Rafhael Gomes <rafhael@protonmail.com>
2023-05-10 17:45:21 -0300

    test: ensures ChangeAccountPasswordUseCase calls hashComparer.compare and throws if it returns false

--- src/app/usecases/change-account-password.spec.ts
+++ src/app/usecases/change-account-password.spec.ts
@@ -6,6 +6,8 @@ import { MissingParamError } from '../errors/missing-param'
 import { AccountRepository } from '@/domain/repositories/account'
 import { Account } from '@/domain/entities/account'
 import { AccountNotFoundError } from '@/app/errors/account-not-found'
+import { WrongPasswordError } from '../errors/wrong-password'
+import { HashComparer } from '../contracts/hash-comparer'
 
 function generateRandomValidPassword(): string {
   const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -76,20 +78,29 @@ class AccountRepositoryStub implements AccountRepository {
   }
 }
 
+class HashComparerStub implements HashComparer {
+  async compare(value: string, hash: string): Promise<boolean> {
+    return true
+  }
+}
+
 interface SutTypes {
   sut: ChangeAccountPassword
   emailValidatorStub: EmailValidator
   accountRepositoryStub: AccountRepository
+  hashComparerStub: HashComparer
 }
 
 function makeSut(): SutTypes {
   const emailValidatorStub = new EmailValidatorStub()
   const accountRepositoryStub = new AccountRepositoryStub()
+  const hashComparerStub = new HashComparerStub()
   const sut = new ChangeAccountPassword(
     emailValidatorStub,
-    accountRepositoryStub
+    accountRepositoryStub,
+    hashComparerStub
   )
-  return { sut, emailValidatorStub, accountRepositoryStub }
+  return { sut, emailValidatorStub, accountRepositoryStub, hashComparerStub }
 }
 
 function makeDTOWithout(
@@ -158,5 +169,33 @@ describe('Change Account Password Use Case', () => {
     const result = sut.execute(dto as ChangeAccountPasswordUseCaseInput)
 
     await expect(result).rejects.toThrow(new AccountNotFoundError())
+  })
+
+  it('should call HashComparer.compare with the correct values', async () => {
+    const { sut, accountRepositoryStub, hashComparerStub } = makeSut()
+    const fakeAccount = makeFakeAccount()
+    jest
+      .spyOn(accountRepositoryStub, 'findByEmail')
+      .mockResolvedValueOnce(fakeAccount)
+    const compareSpy = jest.spyOn(hashComparerStub, 'compare')
+    const dto = makeDTOWithout()
+
+    await sut.execute(dto as ChangeAccountPasswordUseCaseInput)
+
+    expect(compareSpy).toHaveBeenCalledTimes(1)
+    expect(compareSpy).toHaveBeenCalledWith(
+      dto.currentPassword,
+      fakeAccount.password
+    )
+  })
+
+  it('should throw WrongPasswordError if HashComparer.compare returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockResolvedValueOnce(false)
+    const dto = makeDTOWithout()
+
+    const result = sut.execute(dto as ChangeAccountPasswordUseCaseInput)
+
+    await expect(result).rejects.toThrow(new WrongPasswordError())
   })
 })
