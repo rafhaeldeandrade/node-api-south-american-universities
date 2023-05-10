96f5f9c4bcbc190cba8fbdad5a1652b9cb0a2712
Rafhael Gomes <rafhael@protonmail.com>
2023-05-10 11:38:06 -0300

    feat: ensures CreateAccountUseCase throws if name is invalid

--- src/app/usecases/create-account.ts
+++ src/app/usecases/create-account.ts
@@ -4,6 +4,7 @@ import {
   CreateAccountUseCaseOutput,
 } from '@/domain/usecases/create-account'
 import { MissingParamError } from '@/app/errors/missing-param'
+import { InvalidParamError } from '@/app/errors/invalid-param'
 
 export class CreateAccount implements CreateAccountUseCase {
   async execute(
@@ -19,5 +20,16 @@ export class CreateAccount implements CreateAccountUseCase {
     if (!name) throw new MissingParamError('name')
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
+
+    if (name.length < 3) throw new InvalidParamError('name')
+    const isNameValid = this.validateName(name)
+    if (!isNameValid) throw new InvalidParamError('name')
+  }
+
+  validateName(name: string): boolean {
+    const hasNumber = /[0-9]/.test(name)
+    const hasSpecialChar = /[˜!@#$%ˆ&*()_+=`[\]\\}|\\;:"'<,>.?/]/.test(name)
+
+    return !hasNumber && !hasSpecialChar
   }
 }
