14a0cfa94bc829ae60907ccfd3df7de665641048
Rafhael Gomes <rafhael@protonmail.com>
2023-05-10 15:16:12 -0300

    test: implements Encrypter interface in JwtAdapter

---
+++ src/infra/cryptography/jwt/jwt-adapter.spec.ts
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from '@/infra/cryptography/jwt/jwt-adapter'
+import { faker } from '@faker-js/faker'
+
+const fakeValue = faker.datatype.uuid()
+const fakeJwtSecret = faker.datatype.string(128)
+interface SutTypes {
+  sut: JwtAdapter
+}
+function makeSut(): SutTypes {
+  const sut = new JwtAdapter(fakeJwtSecret)
+  return {
+    sut,
+  }
+}
+
+describe('JwtAdapter', () => {
+  it('should be defined', () => {
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
+  })
+
+  it('should have a method called encrypt', () => {
+    const { sut } = makeSut()
+    expect(sut.encrypt).toBeDefined()
+  })
+
+  it('should call jwt.sign with correct params when encrypt method is invoked', () => {
+    const { sut } = makeSut()
+    const jwtSpy = jest.spyOn(jwt, 'sign')
+    sut.encrypt(fakeValue)
+    expect(jwtSpy).toHaveBeenCalledWith(fakeValue, fakeJwtSecret)
+  })
+
+  it('should throw if jwt.sign throws when encrypt method is invoked', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    expect(() => sut.encrypt(fakeValue)).toThrow()
+  })
+})
