2dfa0d054e01c7fe7727e2ba0f9164f9ba828f20
Rafhael Gomes <rafhael@protonmail.com>
2023-05-10 15:15:05 -0300

    feat: implements Encrypter interface in JwtAdapter

---
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '@/app/contracts/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  constructor(private readonly jwtSecret: string) {}
+
+  encrypt(value: string): string {
+    return jwt.sign(value, this.jwtSecret)
+  }
+}
