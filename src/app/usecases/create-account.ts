1b1a9d11ced31ef0974565c23ab3fdb71de5c8bd
Rafhael Gomes <rafhael@protonmail.com>
2023-05-10 15:18:27 -0300

    feat: ensures CreateAccountUseCase is creating a userId and encrypting it

--- src/app/usecases/create-account.ts
+++ src/app/usecases/create-account.ts
@@ -6,15 +6,19 @@ import {
 import { MissingParamError } from '@/app/errors/missing-param'
 import { InvalidParamError } from '@/app/errors/invalid-param'
 import { AccountRepository } from '@/domain/repositories/account'
-import { EmailAlreadyExistsError } from '../errors/email-already-exists'
+import { EmailAlreadyExistsError } from '@/app/errors/email-already-exists'
 import { EmailValidator } from '@/infra/contracts'
 import { Hasher } from '@/app/contracts/hasher'
+import { Encrypter } from '@/app/contracts/encrypter'
+import { UUIDGenerator } from '@/app/contracts/uuid-generator'
 
 export class CreateAccount implements CreateAccountUseCase {
   constructor(
     private readonly emailValidator: EmailValidator,
     private readonly accountRepository: AccountRepository,
-    private readonly hasher: Hasher
+    private readonly uuidGenerator: UUIDGenerator,
+    private readonly hasher: Hasher,
+    private readonly encrypter: Encrypter
   ) {}
 
   async execute(
@@ -25,11 +29,17 @@ export class CreateAccount implements CreateAccountUseCase {
     const account = await this.accountRepository.findByEmail(dto.email)
     if (account) throw new EmailAlreadyExistsError()
 
+    const userId = this.uuidGenerator.generateUUID()
     const hashedPassword = await this.hasher.hash(dto.password)
 
     await this.accountRepository.save({
       ...dto,
+      id: userId,
       password: hashedPassword,
+    })
+
+    this.encrypter.encrypt({
+      id: userId,
     })
 
     return {
--- src/domain/repositories/account.ts
+++ src/domain/repositories/account.ts
@@ -2,5 +2,5 @@ import { Account } from '@/domain/entities/account'
 
 export interface AccountRepository {
   findByEmail(email: string): Promise<Account | null>
-  save(account: Account): Promise<void>
+  save(account: Account): Promise<Account>
 }
