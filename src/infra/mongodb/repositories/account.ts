7d0acab89b5b500b6e1522d30309fc678a066f80
Rafhael Gomes <rafhael@protonmail.com>
2023-05-11 09:46:27 -0300

    feat: implements mongodb account repository

---
+++ src/infra/mongodb/repositories/account.ts
@@ -0,0 +1,31 @@
+import { Account } from '@/domain/entities/account'
+import { AccountRepository } from '@/domain/repositories/account'
+import { AccountModel } from '../schemas/account'
+
+export class MongoDBAccountRepository implements AccountRepository {
+  async findByEmail(email: string): Promise<Account | null> {
+    const account = await AccountModel.findOne({ email }, {}, { lean: true })
+
+    if (!account) return null
+
+    return account
+  }
+
+  async save(account: Account): Promise<Account> {
+    const newAccount = new AccountModel(account)
+    await newAccount.save()
+
+    return account
+  }
+
+  async findByIdAndUpdate(
+    id: string,
+    dataToUpdate: Partial<Account>
+  ): Promise<void> {
+    await AccountModel.findByIdAndUpdate(
+      id,
+      { ...dataToUpdate },
+      { lean: true }
+    )
+  }
+}
