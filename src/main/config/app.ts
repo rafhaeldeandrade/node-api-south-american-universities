6ec6b57512e69d54e1bdc7ade761b65ff7a0438d
Rafhael Gomes <rafhael@protonmail.com>
2023-05-13 15:58:28 -0300

    feat: adds 404 route for when user hit an invalid route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,10 @@
-import express, { json, urlencoded } from 'express'
+import express, {
+  NextFunction,
+  Request,
+  Response,
+  json,
+  urlencoded,
+} from 'express'
 import cors from 'cors'
 import { setupRoutes } from '@/main/config/routes'
 
@@ -9,5 +15,12 @@ app.use(json({ strict: false }))
 app.use(urlencoded({ extended: false }))
 app.use(cors())
 setupRoutes(app).catch(console.error)
+app.use((_req: Request, res: Response, _next: NextFunction) => {
+  res.status(404).json({
+    error: true,
+    message:
+      'Route not found or method not applicable, refer to documentation at https://southamericanuniversities.docs.apiary.io/ to see which routes and methods are available for now.',
+  })
+})
 
 export { app }
