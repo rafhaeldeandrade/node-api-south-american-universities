4ffaae1d74c1169a9b2de9d5d36d486ed4503592
Rafhael Gomes <rafhael@protonmail.com>
2023-05-13 17:31:19 -0300

    test: removes validations from use case

--- src/app/usecases/account/create-account.ts
+++ src/app/usecases/account/create-account.ts
@@ -24,8 +24,6 @@ export class CreateAccount implements CreateAccountUseCase {
   async execute(
     dto: CreateAccountUseCaseInput
   ): Promise<CreateAccountUseCaseOutput> {
-    this.validateDTO(dto)
-
     const account = await this.accountRepository.findByEmail(dto.email)
     if (account) throw new EmailAlreadyExistsError()
 
@@ -46,41 +44,5 @@ export class CreateAccount implements CreateAccountUseCase {
       name: dto.name,
       email: dto.email,
     }
-  }
-
-  validateDTO(dto: CreateAccountUseCaseInput) {
-    const { name, email, password } = dto
-
-    if (!name) throw new MissingParamError('name')
-    if (!email) throw new MissingParamError('email')
-    if (!password) throw new MissingParamError('password')
-
-    const isNameValid = this.validateName(name)
-    if (!isNameValid) throw new InvalidParamError('name')
-
-    const isEmailValid = this.emailValidator.isValid(email)
-    if (!isEmailValid) throw new InvalidParamError('email')
-
-    const isPasswordValid = this.validatePassword(password)
-    if (!isPasswordValid) throw new InvalidParamError('password')
-  }
-
-  validateName(name: string): boolean {
-    return name.length >= 3
-  }
-
-  validatePassword(password: string): boolean {
-    const hasUppercase = /[A-Z]/.test(password)
-    const hasLowercase = /[a-z]/.test(password)
-    const hasNumber = /[0-9]/.test(password)
-    const hasSpecialChar = /[.!@#$%^&*()]/.test(password)
-
-    return (
-      hasUppercase &&
-      hasLowercase &&
-      hasNumber &&
-      hasSpecialChar &&
-      password.length >= 8
-    )
   }
 }
