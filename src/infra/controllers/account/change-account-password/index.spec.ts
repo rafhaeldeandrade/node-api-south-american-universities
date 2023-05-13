c988211ac4bbc3648cdc4acf6e1b79a06325e420
Rafhael Gomes <rafhael@protonmail.com>
2023-05-13 18:04:03 -0300

    test: uses zod to validate body in change account password controller

--- src/infra/controllers/account/change-account-password/index.spec.ts
+++ src/infra/controllers/account/change-account-password/index.spec.ts
@@ -1,14 +1,18 @@
-import { HttpRequest } from '@/infra/contracts'
+import { HttpRequest, SchemaValidator } from '@/infra/contracts'
 import { faker } from '@faker-js/faker'
 import { ChangeAccountPasswordController } from '@/infra/controllers/account/change-account-password'
 import {
   ChangeAccountPasswordUseCase,
   ChangeAccountPasswordUseCaseOutput,
 } from '@/domain/usecases/account/change-account-password'
-import { MissingParamError } from '@/app/errors/missing-param'
-import { InvalidParamError } from '@/app/errors/invalid-param'
 import { AccountNotFoundError } from '@/app/errors/account-not-found'
 import { WrongPasswordError } from '@/app/errors/wrong-password'
+
+class SchemaValidatorStub implements SchemaValidator {
+  async validate(input: any): Promise<Error | null> {
+    return null
+  }
+}
 
 class ChangeAccountPasswordUseCaseStub implements ChangeAccountPasswordUseCase {
   async execute() {
@@ -20,16 +24,19 @@ class ChangeAccountPasswordUseCaseStub implements ChangeAccountPasswordUseCase {
 
 interface SutTypes {
   sut: ChangeAccountPasswordController
+  schemaValidatorStub: SchemaValidator
   changeAccountPasswordUseCaseStub: ChangeAccountPasswordUseCase
 }
 
 function makeSut(): SutTypes {
+  const schemaValidatorStub = new SchemaValidatorStub()
   const changeAccountPasswordUseCaseStub =
     new ChangeAccountPasswordUseCaseStub()
   const sut = new ChangeAccountPasswordController(
+    schemaValidatorStub,
     changeAccountPasswordUseCaseStub
   )
-  return { sut, changeAccountPasswordUseCaseStub }
+  return { sut, schemaValidatorStub, changeAccountPasswordUseCaseStub }
 }
 
 function makeRequest(): HttpRequest {
@@ -48,127 +55,139 @@ function makeUseCaseReturn(): ChangeAccountPasswordUseCaseOutput {
   }
 }
 
-describe('Change Account Password Controller', () => {
+describe('Login Controller', () => {
   afterEach(() => {
     jest.restoreAllMocks()
   })
 
-  it('should call LoginUseCase.execute with the correct values', async () => {
+  it('should call schemaValidator.validate with the correct values', async () => {
+    const { sut, schemaValidatorStub } = makeSut()
+    const request = makeRequest()
+    const validateSpy = jest.spyOn(schemaValidatorStub, 'validate')
+
+    await sut.handle(request)
+
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+
+  it('should return 400 if schemaValidator.validate returns an error', async () => {
+    const { sut, schemaValidatorStub } = makeSut()
+    const request = makeRequest()
+    const error = new Error('teste')
+    jest.spyOn(schemaValidatorStub, 'validate').mockResolvedValueOnce(error)
+
+    const promise = sut.handle(request)
+
+    await expect(promise).resolves.toEqual({
+      statusCode: 400,
+      body: {
+        error: true,
+        message: error.message,
+      },
+    })
+  })
+
+  it('should return 500 if schemaValidator.validate throws an error', async () => {
+    const { sut, schemaValidatorStub } = makeSut()
+    const request = makeRequest()
+    jest
+      .spyOn(schemaValidatorStub, 'validate')
+      .mockRejectedValueOnce(new Error())
+
+    const promise = sut.handle(request)
+
+    await expect(promise).resolves.toEqual({
+      statusCode: 500,
+      body: {
+        error: true,
+        message: 'Internal Server Error',
+      },
+    })
+  })
+
+  it('should call addUniversityUseCase.execute with the correct values', async () => {
     const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const executeSpy = jest.spyOn(changeAccountPasswordUseCaseStub, 'execute')
+    const addSpy = jest.spyOn(changeAccountPasswordUseCaseStub, 'execute')
     const request = makeRequest()
 
     await sut.handle(request)
 
-    expect(executeSpy).toHaveBeenCalledTimes(1)
-    expect(executeSpy).toHaveBeenCalledWith(request.body)
-  })
-
-  it('should call LoginUseCase.execute with the correct values when request has no body', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const executeSpy = jest.spyOn(changeAccountPasswordUseCaseStub, 'execute')
-    const request = makeRequest()
-    const requestWithNoBody = { ...request, body: undefined }
-
-    await sut.handle(requestWithNoBody)
-
-    expect(executeSpy).toHaveBeenCalledTimes(1)
-    expect(executeSpy).toHaveBeenCalledWith({})
-  })
-
-  it('should return 200 with the correct value on success', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const request = makeRequest()
-    const fakeUseCaseReturn = makeUseCaseReturn()
-    jest
-      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
-      .mockResolvedValueOnce(fakeUseCaseReturn)
-
-    const httpResponse = await sut.handle(request)
-
-    expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual(fakeUseCaseReturn)
-  })
-
-  it('should return 400 if useCase throws MissingParamError', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const request = makeRequest()
-    const paramName = faker.internet.userName()
-    jest
-      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
-      .mockRejectedValueOnce(new MissingParamError(paramName))
-
-    const httpResponse = await sut.handle(request)
-
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual({
-      error: true,
-      message: `${paramName} param is missing`,
+    expect(addSpy).toHaveBeenCalledTimes(1)
+    expect(addSpy).toHaveBeenCalledWith({
+      email: request?.body?.email,
+      currentPassword: request?.body?.currentPassword,
+      newPassword: request?.body?.newPassword,
     })
   })
 
-  it('should return 400 if useCase throws InvalidParamError', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const request = makeRequest()
-    const paramName = faker.internet.userName()
-    jest
-      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
-      .mockRejectedValueOnce(new InvalidParamError(paramName))
-
-    const httpResponse = await sut.handle(request)
-
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual({
-      error: true,
-      message: `${paramName} param is invalid`,
-    })
-  })
-
-  it('should return 401 if useCase throws AccountNotFoundError', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const request = makeRequest()
-    jest
-      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
-      .mockRejectedValueOnce(new AccountNotFoundError())
-
-    const httpResponse = await sut.handle(request)
-
-    expect(httpResponse.statusCode).toBe(401)
-    expect(httpResponse.body).toEqual({
-      error: true,
-      message: 'Wrong credentials',
-    })
-  })
-
-  it('should return 401 if useCase throws WrongPasswordError', async () => {
-    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
-    const request = makeRequest()
-    jest
-      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
-      .mockRejectedValueOnce(new WrongPasswordError())
-
-    const httpResponse = await sut.handle(request)
-
-    expect(httpResponse.statusCode).toBe(401)
-    expect(httpResponse.body).toEqual({
-      error: true,
-      message: 'Wrong credentials',
-    })
-  })
-
-  it('should return 500 if useCase throws another Error not covered yet', async () => {
+  it('should return 500 if use case throws an error', async () => {
     const { sut, changeAccountPasswordUseCaseStub } = makeSut()
     const request = makeRequest()
     jest
       .spyOn(changeAccountPasswordUseCaseStub, 'execute')
       .mockRejectedValueOnce(new Error())
 
-    const httpResponse = await sut.handle(request)
+    const promise = sut.handle(request)
 
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual({
-      error: true,
-      message: `Internal Server Error`,
+    await expect(promise).resolves.toEqual({
+      statusCode: 500,
+      body: {
+        error: true,
+        message: 'Internal Server Error',
+      },
+    })
+  })
+
+  it('should return 401 if use case throws AccountNotFoundError', async () => {
+    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
+    const request = makeRequest()
+    jest
+      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new AccountNotFoundError())
+
+    const promise = sut.handle(request)
+
+    await expect(promise).resolves.toEqual({
+      statusCode: 401,
+      body: {
+        error: true,
+        message: 'Wrong credentials',
+      },
+    })
+  })
+
+  it('should return 401 if use case throws WrongPasswordError', async () => {
+    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
+    const request = makeRequest()
+    jest
+      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new WrongPasswordError())
+
+    const promise = sut.handle(request)
+
+    await expect(promise).resolves.toEqual({
+      statusCode: 401,
+      body: {
+        error: true,
+        message: 'Wrong credentials',
+      },
+    })
+  })
+
+  it('should return 200 on success', async () => {
+    const { sut, changeAccountPasswordUseCaseStub } = makeSut()
+    const fakeUseCaseResponse = makeUseCaseReturn()
+    jest
+      .spyOn(changeAccountPasswordUseCaseStub, 'execute')
+      .mockResolvedValueOnce(fakeUseCaseResponse)
+    const request = makeRequest()
+
+    const promise = sut.handle(request)
+
+    await expect(promise).resolves.toEqual({
+      statusCode: 200,
+      body: fakeUseCaseResponse,
     })
   })
 })
