3a1609b43728e51ac089ba7e9d5b441933834971
Rafhael Gomes <rafhael@protonmail.com>
2023-05-13 16:09:08 -0300

    feat: adds express-rate-limit lib

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -6,15 +6,25 @@ import express, {
   urlencoded,
 } from 'express'
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
 import { setupRoutes } from '@/main/config/routes'
 
 export const bodyParser = json({ strict: false })
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+})
 
 const app = express()
 app.use(json({ strict: false }))
 app.use(urlencoded({ extended: false }))
 app.use(cors())
+app.use(limiter)
+
 setupRoutes(app).catch(console.error)
+
 app.use((_req: Request, res: Response, _next: NextFunction) => {
   res.status(404).json({
     error: true,
